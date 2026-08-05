765b086025dc045125821c04c45dff8e0bd4079a
Fernando <fhermosqueira@gmail.com>
2026-08-05 14:10:42 -0300

    refactored example.spec imports

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,12 +1,6 @@
 import { expect } from '@playwright/test';
 import { test } from '../fixtures/pages';
-import { LoginPage } from '../pages/login-page';
-import { AddElementPage } from '../pages/addelement-page';
 
-//test.beforeAll(async ({page}) => {
-//  await page.goto(URL)
-//
-//});
 
 test('succesful login', async ({ loginPage }) => {
   await loginPage.goToLoginPage();
